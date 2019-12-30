bc70f5450c04c7a68d1ec441eba63b99e4fdfe4c
Verenasworld <verena.felizidas@gmail.com>
2019-12-30 23:46:53 +0100

    set up HttpClientModule

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +18,7 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+  constructor (
+    private http: HttpClient
+  ){}
 }
