e8859fb826fcec75b1406c9b00c916476b48d8ca
Verenasworld <verena.felizidas@gmail.com>
2019-12-31 11:18:22 +0100

    import & Inject Form Builder

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 import { CartService, } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
+/*
+definieren der eigenschaft 
+*/
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
 
 /*
 injizieren 
 */
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, 
+  private formBuilder: FormBuilder,
+  ) {
+
+   }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      adress: ''
+    });
   }
 
 }
